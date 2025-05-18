ddb655b6ef85fddc8c8a642c70f6f61758d4630b
MAnsabRaza <ansabnasir153@gmail.com>
2025-05-18 12:47:48 +0500

    1

--- public/index.php
+++ public/index.php
@@ -1,55 +1,52 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ */
 
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+// Ensure the server is using PHP 8
+if (version_compare(PHP_VERSION, '8.0.0', '<')) {
+    die('PHP >= 8.0.0 is required');
 }
+
+// Vercel environment fix
+if (isset($_ENV['VERCEL_REGION'])) {
+    $_SERVER['SCRIPT_NAME'] = '/api/index.php';
+}
+
+// Define the application root directory
+define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
 |--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
 */
+require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__.'/../vendor/autoload.php';
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+*/
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Vercel tmp directory setup for cache
+if (isset($_ENV['VERCEL_REGION'])) {
+    $app->useStoragePath('/tmp');
+}
 
 /*
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
 */
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
 
 $kernel->terminate($request, $response);
